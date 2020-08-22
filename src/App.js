fca1cc3adbe2139e6d6dcd12b28ee7160b55f658
Robert Balonek <robertbalonek@gmail.com>
2020-08-22 10:29:49 -0400

    Starting on Top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import NavBar from "./Components/NavBar/NavBar"
+
 function App() {
   return (
     <div className="App">
-      <h1>HI</h1>
+      <header className="App-header">
+        <h1 className="glow">Robert Balonek</h1>
+      <h2>Software Engineer</h2>
+      </header>
+      
+      <NavBar />
+     
+      
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./NavBar.css"
+
+export default function NavBar() {
+  return (
+    <div className="nav-bar">
+      <div className="container">
+      <p>Thing</p>
+      <p>Thing</p>
+        <p>Thing</p>
+        </div>
+    </div>
+  )
+}
