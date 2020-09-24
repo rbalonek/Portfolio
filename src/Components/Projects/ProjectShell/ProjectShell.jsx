06f2b2c2d6b1273e64cfee38030ccb406d55d7f1
Robert Balonek <robertbalonek@Roberts-MacBook-Pro.local>
2020-09-24 09:24:25 -0400

    Adjusted VH changes with navbar click

--- src/Components/Projects/ProjectShell/ProjectShell.jsx
+++ src/Components/Projects/ProjectShell/ProjectShell.jsx
@@ -1,38 +1,38 @@
-import React from "react";
-import "./ProjectShell.css";
+// import React from "react";
+// import "./ProjectShell.css";
 
- //icons
-import GithubCorner from "react-github-corner";
+//  //icons
+// import GithubCorner from "react-github-corner";
 
-export default function Project(props) {
-  return (
-    <div className="project_container">
-      <div className="project_container__header">
-        <GithubCorner
-          href={props.github}
-          bannerColor="#151513"
-          octoColor="#fff"
-          size={80}
-          direction="left"
-        />
-        <h1>{props.appName}</h1>
-        <h2>Go to Site > </h2>
-      </div>
-      <div className="project_container__image-and-description">
-        <div className="project_container__image-and-description-left">
-          <img src={props.pic} alt="" />
-        </div>
-        <div className="project_container__image-and-description-right">
-          <div className="project_container__image-and-description-right-description">
-            <p>{props.description}</p>
-          </div>
-        </div>
-      </div>
-      <div className="project_container__footer">
-        <img src={props.imageOne} alt="" />
-        <img src={props.imageTwo} alt="" />
-        <img src={props.imageThree} alt="" />
-      </div>
-    </div>
-  );
-}
+// export default function Project(props) {
+//   return (
+//     <div className="project_container">
+//       <div className="project_container__header">
+//         <GithubCorner
+//           href={props.github}
+//           bannerColor="#151513"
+//           octoColor="#fff"
+//           size={80}
+//           direction="left"
+//         />
+//         <h1>{props.appName}</h1>
+//         <h2>Go to Site > </h2>
+//       </div>
+//       <div className="project_container__image-and-description">
+//         <div className="project_container__image-and-description-left">
+//           <img src={props.pic} alt="" />
+//         </div>
+//         <div className="project_container__image-and-description-right">
+//           <div className="project_container__image-and-description-right-description">
+//             <p>{props.description}</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="project_container__footer">
+//         <img src={props.imageOne} alt="" />
+//         <img src={props.imageTwo} alt="" />
+//         <img src={props.imageThree} alt="" />
+//       </div>
+//     </div>
+//   );
+// }
