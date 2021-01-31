225b756970cbb6d93c10478bded3246160d6a3b2
bob <robertbalonek@gmail.com>
2021-01-31 11:00:46 -0800

    Universal Proj Header

--- src/Components/Projects/Cherry/Cherry.js
+++ src/Components/Projects/Cherry/Cherry.js
@@ -1,6 +1,17 @@
 import React from "react";
+import ProjectHeader from "../../Header/ProjectHeader/ProjectHeader";
 import "./Cherry.css";
 
 export default function Cherry() {
-  return <div id="cherry" className="cherry-container"></div>;
+  return (
+    <div id="cherry" className="cherry-container">
+      <ProjectHeader
+        navUp={"happy-space"}
+        navDown={"about"}
+        github={"https://github.com/rbalonek/cherry"}
+        title={"Cherry Project"}
+        website={"https://cherry-site.netlify.app/"}
+      />
+    </div>
+  );
 }
